9be31238dde25e75bd3ecdbca8544ded8505438e
thuriya <paralesely@yahoo.com>
2019-12-03 15:46:54 +0630

    Ready to Show on Laptop!

--- src/model/nqueen/view/Test.java
+++ src/model/nqueen/view/Test.java
@@ -12,11 +12,12 @@ public class Test extends JFrame{
         StoreResult result=new StoreResult();
         ArrayList data=result.getResult();
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        this.setSize(700,750);
+        this.setSize(900,750);
         Dimension dim= Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2,dim.height/2-this.getSize().height/2);
 
-        GridLayout grid=new GridLayout(5,3,10,10);
+        GridLayout grid=new GridLayout(4,2,10,10);
+        
         this.setLayout(grid);
 
         JTextArea[] arr=new JTextArea[data.size()];
@@ -24,9 +25,14 @@ public class Test extends JFrame{
         for(int i=0;i<arr.length;i++)
         {
             arr[i]=new JTextArea(data.get(i).toString());
-            arr[i].setSize(30,30);
+            arr[i].setSize(dim.width/2,30);
             arr[i].setFont(new Font("Verdana",Font.PLAIN, 16));
-            this.add(arr[i]);
+
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            arr[i].setSize(screenSize.width/2, screenSize.height/2);
+
+            JScrollPane sp = new JScrollPane(arr[i]);
+            this.add(sp);
         }
 
         this.setVisible(true);
